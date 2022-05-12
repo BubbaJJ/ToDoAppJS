aab76e0ca4668052821ba76bbc7b908adff10b89
Jim Kaya <Jim.kaya@gmail.com>
2022-05-12 11:22:17 +0200

    WIP

--- js/script.js
+++ js/script.js
@@ -5,22 +5,28 @@ const tasks = document.getElementById("tasks");
 // getTasks
 const getTasks = () => {
   let allTasks = localStorage.getItem("taskList");
-  allTasks = allTasks.split(",");
-  console.log(allTasks);
-  tasks.innerHTML = allTasks
-    .map(
-      (task) => `
-    <div class="task">
-    <div class="content">
-    <input class="text" type="text" readonly="readonly" value="${task}">
-    </div>
-    <div class="actions">
-    <button class="delete">Delete</button>
-    </div>
-    </div>
-    `
-    )
-    .join("");
+  if (allTasks != "") {
+    allTasks = allTasks.split(",");
+    tasks.innerHTML = allTasks
+      .map(
+        (element, index) => `
+      <div class="task">
+      <div class="content">
+      <input class="text" type="text" readonly value="${element}">
+      </div>
+      <div class="actions">
+      <button class="delete" onclick="deleteTask(${index})">Delete</button>
+      </div>
+      </div>
+      `
+      )
+      .join("");
+  } else {
+    tasks.innerHTML = `
+    <div class="no-tasks-found">
+    <h2>No tasks found</h2>
+    </div>`;
+  }
 };
 
 // addTask
@@ -32,6 +38,13 @@ const addTask = (task) => {
 };
 
 // deleteTask
+const deleteTask = (index) => {
+  let allTasks = localStorage.getItem("taskList"); // Hämtar alla element i localStorage.
+  allTasks = allTasks.split(","); // Delar upp textsträngen till en array av strängar.
+  allTasks.splice(index, 1); // Tar bort elementet vid specifikt index.
+  localStorage.setItem("taskList", allTasks); // Sparar alla element till localStorage.
+  getTasks();
+};
 
 // Form submit
 form.onsubmit = (event) => {
